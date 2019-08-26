4604e948e6f40a388a9f325241b2e58a0272b5f5
Davis Grininger <davisgrininger@Daviss-MacBook-Pro-3.local>
2019-08-26 16:53:36 -0700

    fixed things

--- static/js/app.js
+++ static/js/app.js
@@ -1,51 +1,36 @@
-// from data.js
 var tableData = data;
+var button = d3.select("button")
 
-// YOUR CODE HERE!
-var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime");
-var tbody = d3.select("tbody");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+function filterData() {
 
-var populate = (dataInput) => {
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var filtered = data.filter(ufo => ufo.datetime === inputValue);
+  var filterType = d3.select("#selFilter").property("value");
 
-  dataInput.forEach(ufo_sightings => {
-    var row = tbody.append("tr");
-    columns.forEach(column => row.append("td").text(ufo_sightings[column])
-    )
-  });
+
+  if (filterType === "datetime") {
+    filtered = data.filter(ufo => ufo.datetime === inputValue);
+  } else if (filterType === "city") {
+    filtered = data.filter(ufo => ufo.city === inputValue);
+  } else if (filterType === "state") {
+    filtered = data.filter(ufo => ufo.state === inputValue);
+  }
+
+  buildTable(filtered);
 }
 
-//Populate table
-populate(data);
+function buildTable(filtered) {
+  var table = d3.select("#ufo-table");
+ 
 
-// Filter by attribute
-button.on("click", () => {
-  d3.event.preventDefault();
-  var inputDate = inputField1.property("value").trim();
-  // Filter by field matching input value
-  var filterDate = data.filter(data => data.datetime === inputDate);
-  console.log(filterDate)
+  var rows = table.select("tbody").selectAll("tr").data(filtered);
+  rows.enter()
+    .append("tr")
+    .merge(rows)
+    .html(data => `<td>${data.datetime}</td><td>${data.city}</td><td>${data.state}</td><td>${data.country}</td><td>${data.shape}</td><td>${data.durationMinutes}</td><td>${data.comments}</td>`);
+  rows.exit().remove();
+}
 
-  var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
-  console.log(filterData)
-
-  // Add filtered sighting to table
-  tbody.html("");
-
-  let response = {
-    filterData, filterDate
-  }
-
-  if (response.filterData.length !== 0) {
-    populate(filterData);
-  }
-    else if (response.filterData.length === 0 &&  response.filterDate.length !== 0){
-      populate(filterDate);
-  
-    }
-    else {
-      tbody.append("tr").append("td").text("No results found!"); 
-    }
-})
-
+buildTable(data);
+button.on("click", filterData);
